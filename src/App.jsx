14aa06939a4ce402777ec7fb0bce5daedaf39269
Billy Wong <bwong.yh@protonmail.com>
2023-03-11 08:57:30 +0900

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./components/nav/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <div className="flex bg-slate-500">
+      <p>Sign Out</p>
+    </div>
+  );
+};
+
+export default Nav;
